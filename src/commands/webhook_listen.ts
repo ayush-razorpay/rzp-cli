ee1b9e58a489acc6e3a6a53f93612b790887bd4f
Ayush Vipul <ayush.vipul@razorpay.com>
2021-05-22 16:37:05 +0530

    get saved json method ready

--- src/commands/webhook_listen.ts
+++ src/commands/webhook_listen.ts
@@ -4,14 +4,14 @@ import { RzpAuthConfigModel } from "../lib/RzpAuthConfigModel";
 import { RzpApiAuthUtil } from "../lib/RzpApiAuthUtil";
 import { LocalServer } from "../lib/LocalServer";
 import { TunnelingService } from "../lib/TunnelingService";
-const showBanner = require('node-banner');
+const showBanner = require("node-banner");
 const validUrl = require("valid-url");
 
 export default class WebhookListen extends Command {
   static description = "describe the command here";
+
   rzpApiAuthUtil = new RzpApiAuthUtil(this.config.configDir);
-  //rzpApiAuthUtil = new RzpApiAuthUtil('dasdd');
-  apiKeys : any;
+  apiKeys: any;
   static flags = {
     help: flags.help({ char: "h" }),
 
@@ -29,7 +29,6 @@ export default class WebhookListen extends Command {
 
   async run() {
     const { args, flags } = this.parse(WebhookListen);
-
     const url = flags.url;
 
     if (!validUrl.isUri(url)) {
@@ -37,16 +36,17 @@ export default class WebhookListen extends Command {
     }
 
     cli.action.start("Fetching api keys conf ");
-     this.apiKeys = await this.rzpApiAuthUtil.readApiKeysFromConfFile()
-    .then(function(data){
-      cli.action.stop('api keys fetched');
-       
-      return new RzpAuthConfigModel(data.key,data.secret);
-      
-    }).catch(function(err){
-      cli.error('\nfailed to read api keys from path \n\n error :' + err);
-    })
-    
+    this.apiKeys = await this.rzpApiAuthUtil
+      .readApiKeysFromConfFile()
+      .then(function (data) {
+        cli.action.stop("api keys fetched");
+
+        return new RzpAuthConfigModel(data.key, data.secret);
+      })
+      .catch(function (err) {
+        cli.error("\nfailed to read api keys from path \n\n error :" + err);
+      });
+
     cli.action.start("Validating credentials");
     await RzpApiAuthUtil.validateKeys(this.apiKeys.key, this.apiKeys.secret)
       .then(function () {
@@ -54,39 +54,33 @@ export default class WebhookListen extends Command {
       })
       .catch(function () {
         cli.error(
-          "\n Outdate \ Invalid Credentials found inconfig file \n\n" +
+          "\n Outdate / Invalid Credentials found in config file \n\n" +
             "\n to set new api keys run command - rzp-cli login"
         );
       });
-      
 
+    //start a local server
+    cli.action.start("Starting Connection bridge");
+    let tunnel = new TunnelingService(8654);
+    await tunnel.start();
+    cli.action.stop();
 
-      //start a local server 
-      cli.action.start("Starting Connection bridge");
-      let tunnel = new TunnelingService(8654);
-      await tunnel.start();
-      cli.action.stop();
+    let server = new LocalServer(8654, url, this.config.dataDir);
+    await server.core();
 
-      let server = new LocalServer(8654,url);
-      await server.core();
-
-
-      var refreshIntervalId =  setInterval(function(){ 
-      if(server.isSetup && tunnel.isSetup){console.log(' ');
-
-      (async () => {
-        await showBanner('Razorpay', 'Webhook listen started. Forwaring requests to '+ url);
-    })();
-
-      clearInterval(refreshIntervalId);
+    var refreshIntervalId = setInterval(function () {
+      if (server.isSetup && tunnel.isSetup) {
+        (async () => {
+          await showBanner(
+            "Razorpay",
+            "Webhook bridge started.\n Forwaring requests to " + url
+          );
+          cli.action.start('listening ....')
+        })();
+        clearInterval(refreshIntervalId);
       }
       
-  }, 2000);
+    }, 2000);
   }
 
- 
-
-
-
 }
-
--- src/lib/LocalServer.ts
+++ src/lib/LocalServer.ts
@@ -3,41 +3,67 @@ import cli from "cli-ux";
 import { gettimeStamp } from "./CommonUtils";
 const express = require("express");
 const bodyparser = require("body-parser");
+var fsPath = require("fs-path");
+const fs = require("fs");
+const path = require("path");
 
 export class LocalServer {
-
   public isSetup = false;
 
   port: number;
   forwardRequestUrl: string;
   app: any;
-  constructor(port: number, forwardRequestUrl: string) {
+  dataFilePath: string;
+  constructor(port: number, forwardRequestUrl: string, dataFilePath: string) {
     this.forwardRequestUrl = forwardRequestUrl;
     this.port = port;
+    this.dataFilePath = dataFilePath;
     this.app = express();
   }
-
-
   async core() {
     this.app.use(require("body-parser").json());
+
     this.app.post("/", async (req: any, res: any) => {
       let x = req.body;
+      let timeStamp = gettimeStamp();
+      let eventid = jp.query(x, "$..id")[0];
+      let event = x.event;
       cli.info(
-        gettimeStamp() +
-        "-->  [ event : " +
-        x.event +
-        ", id : " +
-        jp.query(x, "$..id")[0] +
-        " ]"
+        timeStamp + "-->  [ event : " + event + ", id : " + eventid + " ]"
       );
-      res.redirect(307, this.forwardRequestUrl)
+
+      let filename = x.event + "-" + eventid + ".json";
+      this.writeFile(x, filename);
+      res.redirect(307, this.forwardRequestUrl);
     });
+
+    this.app.get("/replay/data", async (req: any, res: any) => {
+      await this.getAllPreviousJsonFiles().then((data) => {
+        res.send(data);
+      });
+    });
+
     this.app.listen(this.port, () => {
-      cli.log(
-        `local server listening at http://localhost:${this.port}`
-      );
-          this.isSetup=true;
+      cli.log(`local server listening at http://localhost:${this.port}`);
+      this.isSetup = true;
     });
   }
 
+  writeFile(toSave: any, filename: string) {
+    let path = this.dataFilePath + "/" + filename;
+    fsPath.writeFile(path, JSON.stringify(toSave), function (err: any) {
+      if (err) {
+        cli.error("Failed to write path for Replay on path :" + path);
+      } else {
+        return;
+      }
+    });
+  }
+
+  async getAllPreviousJsonFiles() {
+    return fs
+      .readdirSync(this.dataFilePath + "/")
+      .filter((name: any) => path.extname(name) === ".json")
+      .map((name: any) => require(path.join(this.dataFilePath + "/", name)));
+  }
 }
